2f51523b1aa1c4d2891ccf744e33ebcfeb1221ca
RHrakib1 <rakibhasanmd457@gmail.com>
2024-02-04 20:13:41 +0600

    added to the return

--- return.js
+++ return.js
@@ -19,5 +19,31 @@ function divaid(nums) {
     return divResult;
 }
 divaid(100);
-const divoutput=divaid(100);
+const divoutput = divaid(100);
 console.log(divoutput);
+
+
+
+
+
+
+
+// example 3 return start short vabe return use kora
+console.log("return 3 exaplme start");
+function hottlePrice(price1, price2) {
+    const total = price1 + price2;
+    return total;
+}
+const netTotal = hottlePrice(10, 90);
+console.log(netTotal);
+
+// example short cut return make kora
+console.log("example short cut return make kora");
+function rakib(n, n1, n2) {
+    // const hasan = n + n1 + n2;
+    // return hasan;
+    // sudhu akhane variable declear kora hoi ni
+    return n + n1 + n2;
+}
+const fullname = rakib(5, 10, 90);
+console.log(fullname);
