db4fbadc04da6c35b3ee1650017cfb3152e09f55
RHrakib1 <rakibhasanmd457@gmail.com>
2024-02-04 17:19:36 +0600

    sum to the suction

---
+++ fun.js
@@ -0,0 +1,60 @@
+/**
+ * -----------------function structure------------
+//  *           function declear kora
+ * function khelteJbo(){
+ *            console.log("ghor theke bahir hou")
+ *            console.log("sobaui k dako")
+ *            console.log("hete hete mathe jao")
+ * }
+ * 
+ * 
+ *               function call kora
+ * kelteJbo();
+ */
+// function declear
+function khelteJbo() {
+    console.log("ghor theke bahir hou");
+    console.log("sobaui k dako");
+    console.log("hete hete mathe jao");
+}
+//function call
+khelteJbo();
+
+
+
+
+// examle 2
+console.log("example 2");
+function gosolKrbo() {
+    console.log("bathroom a jabo");
+    console.log("pani sorir a dibo");
+    console.log("saban dibo");
+    console.log("sorir muche bathroom theke jabo");
+}
+gosolKrbo();
+
+
+
+// gun korar niyom
+// bastob example 
+function square(number) {
+    console.log("this is a numnber :", number)
+    const multi = number * number;
+    console.log("this is a multification :", multi)
+}
+square(5);
+console.log("--------------");
+square(8);
+square(6);
+
+
+
+// example fot add 
+function add(num1, num2, num3) {
+    const total = num1 + num2 + num3;
+    console.log("the sum  is :",total);
+
+}
+add(5, 10, 14);
+add(5, 111, 14);
+
